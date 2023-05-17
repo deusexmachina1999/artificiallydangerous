07a7f336a966559e3c44a74160b8b64ea0f472cd
deusexmachina1999 <123769431+deusexmachina1999@users.noreply.github.com>
2023-05-17 06:30:09 +0000

    Make text box work

--- chat.js
+++ chat.js
@@ -15,4 +15,27 @@ function start() {
     body.style.backgroundColor = "black";
 }
 
-//
+// Work on ^
+
+function sendMessage() {
+    console.log("Button pressed.");
+    // Get user input
+    let input = document.getElementById("userResponse");
+    let userData = input.value;
+    input.value = "";
+    console.log(userData)
+
+// make somewhere to put it
+    let text = document.createElement("p");
+
+    // Make user input fill the p made
+    text.innerHTML = userData;
+
+    //change styles
+    text.style.backgroundColor = "darkseagreen";
+    text.style.alignSelf = "flex-end";
+    // append message to the box
+    let messageBox = document.getElementById("chatbot");
+    chatbot.appendChild(text);
+
+}
